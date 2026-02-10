2df81c9df542bc9a2c1eae30e928a13506f97421
armandosouza <armandosouzafalzi@gmail.com>
2026-02-09 22:05:58 -0300

    Commit #3: Funcionalidade de listagem de produtos adicionada

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -42,6 +42,29 @@ public class ProdutosDAO {
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        return listagem;
+        String sql = "SELECT * FROM produtos";
+        conn = new conectaDAO().connectDB();
+        
+        ArrayList<ProdutosDTO> lista = new ArrayList<>();
+        
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while(resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                
+                lista.add(produto);
+            }
+        } catch(SQLException err) {
+            JOptionPane.showMessageDialog(null, "Houve um erro ao listar os produtos!");
+        }
+        
+        return lista;
     }
 }
