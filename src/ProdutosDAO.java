9ad204ea73d126d724a65ac752d53441f6955e51
armandosouza <armandosouzafalzi@gmail.com>
2026-02-11 22:33:33 -0300

    Listagem de produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -88,4 +88,32 @@ public class ProdutosDAO {
             JOptionPane.showMessageDialog(null, "Houve um erro ao realizar a venda! Erro: " + err.getMessage());
         }
     }
+    
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+        
+        conn = new conectaDAO().connectDB();
+        
+        ArrayList<ProdutosDTO> lista = new ArrayList<>();
+        
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while(resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                
+                lista.add(produto);
+            }
+        } catch(SQLException err) {
+            JOptionPane.showMessageDialog(null, "Houve um erro ao listar os produtos! Erro: " + err.getMessage());
+        }
+        
+        return lista;
+    }
 }
